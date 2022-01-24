d9eefb90d1b08f743e1c0e2e8c85d00d7f53d742
sherchryst <sherchryst@icloud.com>
2022-01-24 19:17:37 +0100

    iterator traits

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -6,23 +6,65 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 17:41:56 by sgah              #+#    #+#             */
-/*   Updated: 2021/11/17 17:04:07 by sgah             ###   ########.fr       */
+/*   Updated: 2022/01/24 19:13:31 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+# include <cstddef>
+
 namespace ft
 {
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag: public input_iterator_tag {};
+	struct bidirectional_iterator_tag: public forward_iterator_tag {};
+	struct random_access_iterator_tag: public bidirectional_iterator_tag {};
+
 /**========================================================================
- **                           VALID_ITERATOR
- *?
+ * *                                ITERATOR_TRAITS
+ * ? Traits class defining properties of iterators.
  *========================================================================**/
-	template <bool is_valid, typename T>
-	struct valid_iterator_tag_base
+	template <class Iter>
+	class iterator_traits
 	{
-		typedef T type;
-		const static bool value = is_valid;
+		public:
+			typedef typename Iter::difference_type		difference_type;
+			typedef typename Iter::value_type			value_type;
+			typedef typename Iter::pointer				pointer;
+			typedef typename Iter::reference			reference;
+			typedef typename Iter::iterator_category	iterator_category;
 	};
+
+/**========================================================================
+ * *                                ITERATOR_TRAITS
+ * !                                SPECIALIZATION
+ * ? This type trait may be specialized for user-provided
+ * ? types that may be used as iterators.
+ *========================================================================**/
+	template <class T>
+	class iterator_traits<T*>
+	{
+		public:
+			typedef std::ptrdiff_t						difference_type;
+			typedef T									value_type;
+			typedef T*									pointer;
+			typedef T&									reference;
+			typedef ft::random_access_iterator_tag		iterator_category;
+	};
+
+	template <class T>
+	class iterator_traits<const T*>
+	{
+		public:
+			typedef std::ptrdiff_t						difference_type;
+			typedef T									value_type;
+			typedef const T*							pointer;
+			typedef const T&							reference;
+			typedef ft::random_access_iterator_tag		iterator_category;
+	};
+/*============================ END OF ITERATOR_TRAITS =====================*/
+
 
 }
